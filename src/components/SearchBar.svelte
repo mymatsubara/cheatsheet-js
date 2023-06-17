3c99c4f92425a7d67119ae47973499393cae23dc
Murilo Matsubara <murilo.matsubara@gmail.com>
2023-06-17 17:51:25 -0300

    feat: add search bar

---
+++ src/components/SearchBar.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { searchQuery } from "../scripts/svelte/search-store";
+</script>
+
+<input
+	class="rounded bg-neutral-100 px-3 py-1 text-neutral-700 shadow code-font"
+	bind:value={$searchQuery}
+	type="search"
+/>
--- src/components/cheatsheet/CheatSheet.svelte
+++ src/components/cheatsheet/CheatSheet.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+	import { searchQuery } from "../../scripts/svelte/search-store";
 	import Section from "./Section.svelte";
 	import type { CheatSheetData } from "./types";
 
 	export let sections: CheatSheetData;
+
+	$: console.log($searchQuery);
 </script>
 
 <div class="grid grid-cols-1 lg:grid-cols-2 gap-5 code-font">
