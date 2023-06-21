a546a7bb376cd43280dd4579256377d80aaa771e
Murilo Matsubara <murilo.matsubara@gmail.com>
2023-06-21 01:13:17 -0300

    chore: add github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), svelte()],
+	site: "https://mymatsubara.github.io",
+	base: "/cheatsheet-js"
 });
